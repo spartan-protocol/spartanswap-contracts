becf6b7c8f7491c036e5c6ef8b760f0444e33631
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-08-12 10:18:09 +0800

    minimum initial liqAdd

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -173,7 +173,7 @@ contract Pool is iBEP20, ReentrancyGuard {
         uint256 _actualInputToken = _getAddedTokenAmount(); // Get the received TOKEN amount
         liquidityUnits = iUTILS(_DAO().UTILS()).calcLiquidityUnits(_actualInputBase, baseAmount, _actualInputToken, tokenAmount, totalSupply); // Calculate LP tokens to mint
         if(baseAmount == 0 || tokenAmount == 0){
-            require(_actualInputBase >= 1000 && _actualInputToken >= 1000, "!Balanced");
+            require(_actualInputBase >= (100 * (10**18)) && _actualInputToken >= 10000, "!Balanced");
             uint createFee = 100 * liquidityUnits / 10000;
             liquidityUnits -= createFee;
             _mint(BASE, createFee);
